b61d295fb58c4a63c14e144cfd03ec35387258a3
nellowint <wellintonvieira.office@gmail.com>
2026-07-05 16:07:40 -0300

    fix: add @csrf_exempt in deploy view

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseForbidden
 from django.core import management
+from django.views.decorators.csrf import csrf_exempt
 from decouple import config
 
 
+@csrf_exempt
 def deploy(request, token):
     expected = config('DEPLOY_TOKEN', default='')
     if not expected or token != expected:
